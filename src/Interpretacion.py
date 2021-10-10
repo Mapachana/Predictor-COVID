cd05256bc4a57f6a918f5e21c1d34234dd3ed26e
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-10 11:58:38 +0200

    Corregido Interpretacion.py
    Corregido estilo CamelCase
    Eliminados metodos para acceder a atributos de instancia

--- src/Interpretacion.py
+++ src/Interpretacion.py
@@ -1,14 +1,5 @@
-from datetime import date
-
-
 class Interpretacion:
 
     def __init__(self, datos):
-        self.contenedorDatos = list(datos)
-        self.textoExplicacion = ""
-
-    def contenedorDatos(self):
-        return self.contenedorDatos
-
-    def textoExplicacion(self):
-        return self.textoExplicacion
+        self.ContenedorDatos = list(datos)
+        self.TextoExplicacion = ""
