14723ab26b8d02b4f7e1a1d5eb19dee06258608e
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-10 11:55:36 +0200

    Corregido Datos.py
    Corregido al estilo CamelCase
    Eliminados atributos de clase
    Eliminados metodos para acceder a atributos de instancia

--- src/Datos.py
+++ src/Datos.py
@@ -1,31 +1,10 @@
 from datetime import datetime
 
 class Datos:
-    numCasos = 0
-    ciudad = ""
-    provincia = ""
-    comAutonoma = ""
-    fecha = ""
 
     def __init__(self, numcasos, ciud, prov, comAuto):
-        self.numCasos = numcasos
-        self.ciudad = ciud
-        self.provincia = prov
-        self.comAutonoma = comAuto
-        self.fecha = datetime.today()
-
-    def numCasos(self):
-        return self.numCasos
-
-    def ciudad(self):
-        return self.ciudad
-
-    def provincia(self):
-        return self.provincia
-
-    def comAutonoma(self):
-        return self.comAutonoma
-
-    def fecha(self):
-        return self.fecha
-
+        self.NumCasos = numcasos
+        self.Ciudad = ciud
+        self.Provincia = prov
+        self.ComAutonoma = comAuto
+        self.Fecha = datetime.today()
