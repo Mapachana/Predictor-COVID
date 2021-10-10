b24c170d8448300c190e4a993590f532cc01b0ba
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-10 11:59:30 +0200

    Corregido Prediccion.py
    Corregido estilo CamelCase
    Eliminados metodos para acceder a atributos de instancia

--- src/Prediccion.py
+++ src/Prediccion.py
@@ -1,20 +1,7 @@
-
 class Prediccion:
 
     def __init__(self,datos):
-        self.historialDatos = list(datos)
-        self.numCasosPredict = 0
-        self.prediccion = ""
-        self.nivelAlerta = 0
-
-    def historialDatos(self):
-        return self.historialDatos
-
-    def numCasosPredict(self):
-        return self.numCasosPredict
-
-    def prediccion(self):
-        return self.prediccion
-
-    def nivelAlerta(self):
-        return self.nivelAlerta
+        self.HistorialDatos = list(datos)
+        self.NumCasosPredict = 0
+        self.Prediccion = ""
+        self.NivelAlerta = 0
