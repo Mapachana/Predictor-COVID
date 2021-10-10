bbc82eec71498519e01151b38930dec2aaeca949
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-10 11:57:32 +0200

    Corregido Graficas.py
    Corregido estilo CamelCase
    Eliminados metodos para acceder a atributos de instancia

--- src/Graficas.py
+++ src/Graficas.py
@@ -1,19 +1,10 @@
 class Graficas:
 
     def __init__(self,datos):
-        self.contenedorDatos = list(datos)
-        tam = len(self.contenedorDatos)
-        self.numCasos = list()
-        self.fechas = list()
+        self.ContenedorDatos = list(datos)
+        tam = len(self.ContenedorDatos)
+        self.NumCasos = list()
+        self.Fechas = list()
         for i in range(tam):
-            self.numCasos.append(self.contenedorDatos[i].numCasos)
-            self.fechas.append(self.contenedorDatos[i].fecha)
-
-    def contenedorDatos(self):
-        return self.contenedorDatos
-
-    def numCasos(self):
-        return self.numCasos
-
-    def fechas(self):
-        return self.fechas
+            self.NumCasos.append(self.ContenedorDatos[i].NumCasos)
+            self.Fechas.append(self.ContenedorDatos[i].Fecha)
