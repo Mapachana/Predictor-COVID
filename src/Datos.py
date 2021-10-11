09fa15ce4cc3a53dce5012752e01b493d50b365b
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-11 19:35:32 +0200

    Corregido Datos.py
    Añadido estilo snake-case

--- src/Datos.py
+++ src/Datos.py
@@ -2,9 +2,13 @@ from datetime import datetime
 
 class Datos:
 
-    def __init__(self, numcasos, ciud, prov, comAuto):
-        self.NumCasos = numcasos
-        self.Ciudad = ciud
-        self.Provincia = prov
-        self.ComAutonoma = comAuto
-        self.Fecha = datetime.today()
+    def __init__(self, numcasos, ciud, prov, com_auto):
+        self.num_casos = numcasos
+        self.ciudad = ciud
+        self.provincia = prov
+        self.com_autonoma = com_auto
+        self.fecha = datetime.today()
+
+    # Metodo para descargar los datos en un fichero de texto
+    def exportar_datos_a_fichero(self):
+        pass
