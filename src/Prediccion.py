78ead3b05228b80a2f0480611a081176609782d3
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-13 18:46:40 +0200

    Correccion docstring Prediccion.py

--- src/Prediccion.py
+++ src/Prediccion.py
@@ -3,6 +3,6 @@ class Prediccion:
     def __init__(self,datos):
         self.historial_datos = list(datos)
 
-    # Metodo para generar una prediccion respecto a una comunidad autonoma o a una ciudad en concreto
     def generar_prediccion(self,com_auto,ciudad):
+       ''' Metodo para generar una prediccion respecto a una comunidad autonoma o a una ciudad en concreto '''
         pass
