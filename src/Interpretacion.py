8ab9bbe5d3f9f60aa5bbe116c82761d3e831c8f8
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-13 18:48:47 +0200

    Correccion visual Interpretacion.py

--- src/Interpretacion.py
+++ src/Interpretacion.py
@@ -4,10 +4,8 @@ class Interpretacion:
         self.contenedor_datos = list(datos)
  
     def generar_interpretacion(self,com_auto,ciudad,fecha):
-        ''' 
-        Metodo para crear una interpretacion a partir del contenedor de datos 
+        ''' Metodo para crear una interpretacion a partir del contenedor de datos 
         se puede filtrar dicha interpretacion por comunidad autonoma, ciudad y
         fecha. Si no se desea contar con alguno de esos atributos, 
-        se marca como cadena vacia ""
-        '''   
+        se marca como cadena vacia "" '''   
         pass
