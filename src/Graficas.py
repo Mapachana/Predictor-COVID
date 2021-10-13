c4634bdf024d3a08e1662ade7f6ff930a1aeb753
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-13 18:43:32 +0200

    Corrección docstring Graficas.py

--- src/Graficas.py
+++ src/Graficas.py
@@ -2,9 +2,10 @@ class Graficas:
 
     def __init__(self,datos):
         self.contenedor_datos = list(datos)
-
-    ''' Metodo para crear una gráfica a partir de los parametros
-        parametros posibles: comunidad Autonoma, ciudad, fecha inicio, fecha final ...
-        si no se desea introducir algun parametro se introduce una cadena vacia "" '''
+ 
     def crear_grafica(self,com_auto,ciudad,fecha_ini, fecha_fin):
+        """ Metodo para crear una gráfica a partir de los parametros
+        parametros posibles: comunidad Autonoma, ciudad, fecha inicio, fecha final ...
+        si no se desea introducir algun parametro se introduce una cadena vacia "" 
+        """
         pass
