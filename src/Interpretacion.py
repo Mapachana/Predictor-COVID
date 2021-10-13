dfb44aee69b4dc841a51fd2c1d9a2fcaf196bd55
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-10-13 18:44:40 +0200

    Corregido docstring de Interpretacion.py

--- src/Interpretacion.py
+++ src/Interpretacion.py
@@ -2,10 +2,11 @@ class Interpretacion:
 
     def __init__(self, datos):
         self.contenedor_datos = list(datos)
-
-    ''' Metodo para crear una interpretacion a partir del contenedor de datos 
+ 
+    def generar_interpretacion(self,com_auto,ciudad,fecha):
+        """ Metodo para crear una interpretacion a partir del contenedor de datos 
         se puede filtrar dicha interpretacion por comunidad autonoma, ciudad y
         fecha. Si no se desea contar con alguno de esos atributos, 
-        se marca como cadena vacia "" '''
-    def generar_interpretacion(self,com_auto,ciudad,fecha):
+        se marca como cadena vacia ""
+        """   
         pass
