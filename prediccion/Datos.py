f7e3a60caf34cfbd19174d62c91b5d351e28a009
Mapachana <anabuenrua99@gmail.com>
2021-10-22 13:41:38 +0200

    Cambio orden de atributos por simplicidad

--- prediccion/Datos.py
+++ prediccion/Datos.py
@@ -5,7 +5,7 @@ class Datos:
     Clase para representar los datos de nuevos casos en una comunidad autónoma en un día dado
     '''
 
-    def __init__(self, num_casos, com_auto, fecha):
+    def __init__(self, fecha, com_auto, num_casos):
         '''
         Constructor de la clase Datos.
 
@@ -14,6 +14,6 @@ class Datos:
             com_auto: Comunidad autonoma donde se reportan los casos
             fecha: Dia con los nuevos casos
         '''
-        self.num_casos = num_casos
-        self.com_autonoma = com_auto
         self.fecha = fecha
+        self.com_autonoma = com_auto
+        self.num_casos = num_casos
