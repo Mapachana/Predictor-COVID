b6da243a9a4e53e8be36af7f38df8ccfbdb794a9
Mapachana <anabuenrua99@gmail.com>
2021-10-22 13:31:33 +0200

    closes #10

--- prediccion/Datos.py
+++ prediccion/Datos.py
@@ -1,10 +1,19 @@
 from datetime import datetime
 
 class Datos:
+    '''
+    Clase para representar los datos de nuevos casos en una comunidad autónoma en un día dado
+    '''
 
-    def __init__(self, numcasos, ciud, prov, com_auto):
-        self.num_casos = numcasos
-        self.ciudad = ciud
-        self.provincia = prov
+    def __init__(self, num_casos, com_auto, fecha):
+        '''
+        Constructor de la clase Datos.
+
+        Argumentos:
+            - num_casos: numero de casos nuevos ese dia
+            com_auto: Comunidad autonoma donde se reportan los casos
+            fecha: Dia con los nuevos casos
+        '''
+        self.num_casos = num_casos
         self.com_autonoma = com_auto
-        self.fecha = datetime.today()
+        self.fecha = fecha
