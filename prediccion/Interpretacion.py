f8be3e4978055d91c7ea2592f8e09936c458b6e8
Mapachana <anabuenrua99@gmail.com>
2021-10-22 13:46:49 +0200

    Reformulo clase Interpretacion

--- prediccion/Interpretacion.py
+++ prediccion/Interpretacion.py
@@ -1,11 +1,20 @@
 class Interpretacion:
+    '''
+    Clase para obtener información de un conjunto de Datos
+    '''
 
     def __init__(self, datos):
+        '''
+        Constructor de la clase Interpretacion, que trabaja con los datos para dar una explicacion sencilla de la situacion actual.
+
+        Argumentos:
+            - datos: Lista de datos sobre los que se trabaja 
+        '''
         self.contenedor_datos = list(datos)
  
-    def generar_interpretacion(self,com_auto,ciudad,fecha):
+    def generar_interpretacion(self,com_auto,fecha):
         ''' Metodo para crear una interpretacion a partir del contenedor de datos 
-        se puede filtrar dicha interpretacion por comunidad autonoma, ciudad y
+        se puede filtrar dicha interpretacion por comunidad autonoma y
         fecha. Si no se desea contar con alguno de esos atributos, 
         se marca como cadena vacia "" '''   
         pass
