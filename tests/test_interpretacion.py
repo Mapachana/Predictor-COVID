16ac98d549799cb771119e96d2560a564e279744
Mapachana <anabuenrua99@gmail.com>
2021-11-07 23:08:41 +0100

    Añado documentacion de tests, closes #12

--- tests/test_interpretacion.py
+++ tests/test_interpretacion.py
@@ -8,33 +8,48 @@ sys.path.append('./')
 sys.path.append('./prediccion/')
 
 
-
 from prediccion.interpretacion import Interpretacion
 
 
 @pytest.fixture
 def interp():
+    ''''
+    Funcion fixture que crea el objeto interpretacion a usar para los tests
+    '''
     interp = Interpretacion([])
     interp.leer_datos("./tests/datos_prueba.csv")
 
     return interp
 
 def test_tiene_datos(interp):
+    '''
+    Funcion test para comprobar si el contenedor de datos esta vacio
+    '''
     assert interp.contenedor_datos != []
 
 def test_excepcion():
+    ''''
+    Funcion test para comprobar si se lanza una excepcion de fichero no encontrado al pasar un archivo inexistente
+    '''
     interp = Interpretacion([])
     aux = unittest.TestCase()
     with aux.assertRaises(FileNotFoundError):
         interp.leer_datos("./noexisto.csv")
 
 def test_datos_correctos(interp):
+    '''
+    Funcion test para comprobar si los datos se han leido correctamente y tienen sentido
+    '''
     for elem in interp.contenedor_datos:
         assert elem.fecha != ''
         assert elem.com_autonoma != ''
         assert elem.num_casos >= 0
 
 def test_interpretacion_correcta(interp):
+    '''
+    Funcion test para comprobar si las interpretaciones son correctar
+    '''
+
     # Redirijo la salida
     captured_output = io.StringIO()
     sys.stdout = captured_output
@@ -55,6 +70,10 @@ def test_interpretacion_correcta(interp):
     sys.stdout = sys.__stdout__
     
 def test_interpretacion_ccaa_no_existe(interp):
+    ''''
+    Funcion test para asegurarse de que no realiza predicciones si los datos pedidos no son correctos
+    '''
+
      # Redirijo la salida
     captured_output = io.StringIO()
     sys.stdout = captured_output
