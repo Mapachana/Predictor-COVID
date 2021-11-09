f04558c0d9e58cca07489778a0a85debf9652806
Mapachana <anabuenrua99@gmail.com>
2021-11-09 20:36:04 +0100

    Hago los tests una clase de unittest, closes #12

--- tests/test_interpretacion.py
+++ tests/test_interpretacion.py
@@ -1,7 +1,5 @@
 import sys
 import unittest
-import pytest
-import io
 
 # Configuro paths
 sys.path.append('./')
@@ -10,56 +8,55 @@ sys.path.append('./prediccion/')
 
 from prediccion.interpretacion import Interpretacion
 
-
-@pytest.fixture
-def interp():
-    ''''
-    Funcion fixture que crea el objeto interpretacion a usar para los tests
-    '''
-    interp = Interpretacion("./tests/datos_prueba.csv")
-
-    return interp
-
-def test_tiene_datos(interp):
-    '''
-    Funcion test para comprobar si el contenedor de datos esta vacio
-    '''
-    assert interp.contenedor_datos != []
-
-def test_excepcion():
-    ''''
-    Funcion test para comprobar si se lanza una excepcion de fichero no encontrado al pasar un archivo inexistente
-    '''
-    aux = unittest.TestCase()
-    with aux.assertRaises(SystemExit):
-        interp = Interpretacion("./noexisto.csv")
-
-def test_datos_correctos(interp):
-    '''
-    Funcion test para comprobar si los datos se han leido correctamente y tienen sentido
-    '''
-    for elem in interp.contenedor_datos:
-        assert elem.fecha != ''
-        assert elem.com_autonoma != ''
-        assert elem.num_casos >= 0
-
-def test_interpretacion_correcta(interp):
-    '''
-    Funcion test para comprobar si las interpretaciones son correctar
-    '''
-
-    interpretacion_generada = interp.generar_interpretacion('Andalucía', '2021-01-10')
-    assert interpretacion_generada == "No vamos bien"
-
-    interpretacion_generada = interp.generar_interpretacion("Andalucía", "2020-03-10")
-    assert interpretacion_generada == "Vamos bien"
+class TestInterpretacion(unittest.TestCase):
+    
+    def setUp(self):
+        ''''
+        Funcion fixture que crea el objeto interpretacion a usar para los tests
+        '''
+        self.interp = Interpretacion("./tests/datos_prueba.csv")
 
 
-def test_interpretacion_ccaa_no_existe(interp):
-    ''''
-    Funcion test para asegurarse de que no realiza predicciones si los datos pedidos no son correctos
-    '''
+    def test_tiene_datos(self):
+        '''
+        Funcion test para comprobar si el contenedor de datos esta vacio
+        '''
+        #self.interp = Interpretacion("./tests/datos_prueba.csv")
 
-    interpretacion_generada = interp.generar_interpretacion('CCAANoExistente', '2021-01-10')
-    assert interpretacion_generada == "No se ha encontrado la comunidad autonoma o fecha indicadas"
+        assert self.interp.contenedor_datos != []
 
+    def test_excepcion(self):
+        ''''
+        Funcion test para comprobar si se lanza una excepcion de fichero no encontrado al pasar un archivo inexistente
+        '''
+        with self.assertRaises(SystemExit):
+            interp = Interpretacion("./noexisto.csv")
+
+    def test_datos_correctos(self):
+        '''
+        Funcion test para comprobar si los datos se han leido correctamente y tienen sentido
+        '''
+        for elem in self.interp.contenedor_datos:
+            assert elem.fecha != ''
+            assert elem.com_autonoma != ''
+            assert elem.num_casos >= 0
+
+    def test_interpretacion_correcta(self):
+        '''
+        Funcion test para comprobar si las interpretaciones son correctar
+        '''
+
+        interpretacion_generada = self.interp.generar_interpretacion('Andalucía', '2021-01-10')
+        assert interpretacion_generada == "No vamos bien"
+
+        interpretacion_generada = self.interp.generar_interpretacion("Andalucía", "2020-03-10")
+        assert interpretacion_generada == "Vamos bien"
+
+
+    def test_interpretacion_ccaa_no_existe(self):
+        ''''
+        Funcion test para asegurarse de que no realiza predicciones si los datos pedidos no son correctos
+        '''
+
+        interpretacion_generada = self.interp.generar_interpretacion('CCAANoExistente', '2021-01-10')
+        assert interpretacion_generada == "No se ha encontrado la comunidad autonoma o fecha indicadas"
