55860d081e2fa1650812f7174b079fbbcb3273e8
Mapachana <anabuenrua99@gmail.com>
2021-11-09 17:31:28 +0100

    Hago clase Datos en dataclass

--- prediccion/datos.py
+++ prediccion/datos.py
@@ -1,19 +1,11 @@
-from datetime import datetime
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
 class Datos:
     '''
     Clase para representar los datos de nuevos casos en una comunidad autónoma en un día dado
     '''
 
-    def __init__(self, fecha, com_auto, num_casos):
-        '''
-        Constructor de la clase Datos.
-
-        Argumentos:
-            - num_casos: numero de casos nuevos ese dia
-            com_auto: Comunidad autonoma donde se reportan los casos
-            fecha: Dia con los nuevos casos
-        '''
-        self.fecha = fecha
-        self.com_autonoma = com_auto
-        self.num_casos = num_casos
+    fecha: str
+    com_autonoma: str
+    num_casos: int
