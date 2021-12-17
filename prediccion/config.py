5fade35f3100bb91dce1cc156ba9317cad413074
Mapachana <anabuenrua99@gmail.com>
2021-12-17 10:27:40 +0100

    Cargo configuracion

--- prediccion/config.py
+++ prediccion/config.py
@@ -1,6 +1,8 @@
 import sys
+import os
 import logging
-import dotenv
+from dotenv import load_dotenv, find_dotenv
+from dotenv.main import find_dotenv
 
 # Configuro paths
 sys.path.append('./')
@@ -8,4 +10,11 @@ sys.path.append('./prediccion/')
 
 class Config:
     def __init__(self):
-        pass
+        load_dotenv(find_dotenv())
+        
+        self.FORMAT = os.getenv('FORMAT')
+        self.LOGFILE = os.getenv('LOGFILE')
+        path = os.getenv('LOGFOLDER')
+        os.mkdir(path)
+        self.LEVEL = os.getenv('LEVEL')
+
--- prediccion/gestor_interpretacion.py
+++ prediccion/gestor_interpretacion.py
@@ -7,22 +7,27 @@ sys.path.append('./prediccion/')
 
 import datos as dt
 import interpretacion as it
+import config as config
 
 class GestorInterpretacion:
     '''Clase para gestionar la clase Interpretacion'''
     def __init__(self, fichero):
         '''Constructor de la clase GestorInterpretacion, que gestiona la clase Interpretacion'''
+        # Instancio la configuracion para configurar el logger
+        self.config = config.Config()
+        logging.basicConfig(filename=self.config.LOGFILE, filemode='a', format=self.config.FORMAT, level=self.config.LEVEL)
         self.logger = logging.getLogger()
+
         try:
             self.interpretacion = it.Interpretacion(fichero)
-            self.logger().info("Se crea el objeto de interpretacion")
+            self.logger.info("Se crea el objeto de interpretacion")
         except:
-           self.logger().error("No se ha encontrado el fichero")
+           self.logger.error("No se ha encontrado el fichero")
 
     def generar_interpretacion(self, com_auto, fecha):
         '''Metodo para genear una interpretacion. Recibe como argumentos la comunidad autonoma y fecha'''
         salida = self.interpretacion.generar_interpretacion(com_auto, fecha)
         if salida == it.EstadoSituacion.DESCONOCIDA:
-            self.logger().error("No se ha podido generar la interpretacion")
+            self.logger.error("No se ha podido generar la interpretacion")
         else:
-            self.logger().info("Interpretacion generada correctamente")
+            self.logger.info("Interpretacion generada correctamente")
