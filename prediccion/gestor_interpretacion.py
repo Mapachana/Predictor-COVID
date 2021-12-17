f390b6020226f7e735f9bcc603233af108b0888a
Mapachana <anabuenrua99@gmail.com>
2021-12-17 14:16:31 +0100

    Resubo

--- prediccion/gestor_interpretacion.py
+++ prediccion/gestor_interpretacion.py
@@ -24,7 +24,6 @@ class GestorInterpretacion:
         path = self.config.LOGFILE
         existe = os.path.isfile(self.config.LOGFILE)
         if not existe:
-            sys.exec()
             open(self.config.LOGFILE, 'w+').close()
 
         logging.basicConfig(filename=self.config.LOGFILE, filemode='a', format=self.config.FORMAT, level=self.config.LEVEL)
