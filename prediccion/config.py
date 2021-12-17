1a5deb3beec5c1d9c1b5776022fe9f7b8a7217e7
Mapachana <anabuenrua99@gmail.com>
2021-12-17 09:54:00 +0100

    Añado clase de configuracion

---
+++ prediccion/config.py
@@ -0,0 +1,11 @@
+import sys
+import logging
+import dotenv
+
+# Configuro paths
+sys.path.append('./')
+sys.path.append('./prediccion/')
+
+class ConfigLogging:
+    def __init__(self):
+        pass
